1c33e592fc201837b428e5728ad5ef5328a4d4fd
tesin <tesin0921@gmail.com>
2021-11-19 17:53:41 +0900

    Revert "finish_five-factors"
    This reverts commit 2bc5c803ef29ce021b9a4b1aee6194d5638b6e85.

--- get_data.py
+++
@@ -1,7 +0,0 @@
-from pandas_datareader import data as pdr
-import yfinance as yf
-yf.pdr_override() # <== that's all it takes :-) 
-symbol_AAPL = 'AAPL'
-df_AAPL = pdr.get_data_yahoo(symbol_AAPL, start="2021-09-01", end="2021-11-12")
-import datetime
-#df_AAPL.to_csv("df_price_AAPL_{}.csv".format(datetime.date.today()),index = True)
