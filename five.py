8a4435b5210743fa9aea797664ca1d9639d62a67
tesin <tesin0921@gmail.com>
2021-12-24 13:39:50 +0900

    plot途中

--- five.py
+++ five.py
@@ -10,8 +10,13 @@ from statsmodels.iolib.summary2 import summary_col # module for presenting stats
 class overwrite:
   test = 0
 
+class df_overwrite:
+  df_factors = 0
+
 # predictという変数にoverwriteクラスをインスタンス化
 predict = overwrite
+
+df_predict = df_overwrite
 
 # csvファイルにする関数
 def pdCsv(fileName):
@@ -28,7 +33,7 @@ def price2ret(prices,retType='simple'):
         ret = np.log(prices/prices.shift(1))
     return ret
 
-def assetPriceReg(df_stk):
+def assetPriceReg(df_stk, df_predict):
     import pandas_datareader.data as web  # module for reading datasets directly from the web
     #from datetime import datetime
     #st = datetime(2021, 10, 1)
@@ -36,17 +41,17 @@ def assetPriceReg(df_stk):
     #df_factors = web.DataReader('F-F_Research_Data_5_Factors_2x3_daily', 'famafrench    ', st,ed)[0]
 
     # Reading in factor data
-    df_factors = web.DataReader('F-F_Research_Data_5_Factors_2x3_daily', 'famafrench')[0]
-    df_factors.rename(columns={'Mkt-RF': 'MKT'}, inplace=True)
-    df_factors['MKT'] = df_factors['MKT']/100
-    df_factors['SMB'] = df_factors['SMB']/100
-    df_factors['HML'] = df_factors['HML']/100
-    df_factors['RMW'] = df_factors['RMW']/100
-    df_factors['CMA'] = df_factors['CMA']/100
-    df_factors['One'] = 1
-    print(df_factors)
+    df_predict.df_factors = web.DataReader('F-F_Research_Data_5_Factors_2x3_daily', 'famafrench')[0]
+    df_predict.df_factors.rename(columns={'Mkt-RF': 'MKT'}, inplace=True)
+    df_predict.df_factors['Intercept'] = 1
+    df_predict.df_factors['MKT'] = df_predict.df_factors['MKT']/100
+    df_predict.df_factors['SMB'] = df_predict.df_factors['SMB']/100
+    df_predict.df_factors['HML'] = df_predict.df_factors['HML']/100
+    df_predict.df_factors['RMW'] = df_predict.df_factors['RMW']/100
+    df_predict.df_factors['CMA'] = df_predict.df_factors['CMA']/100
+    #print(df_predict.df_factors)
 
-    df_stock_factor = pd.merge(df_stk,df_factors,left_index=True,right_index=True) 
+    df_stock_factor = pd.merge(df_stk,df_predict.df_factors,left_index=True,right_index=True) 
     # Merging the stock and factor returns dataframes together
     df_stock_factor['XsRet'] = df_stock_factor['Returns'] - df_stock_factor['RF'] # Calculating excess returns
 
@@ -66,8 +71,7 @@ def assetPriceReg(df_stk):
     # DataFrame with coefficients and t-stats
     results_df = pd.DataFrame({'CAPMcoeff':CAPMcoeff,'CAPMtstat':CAPMtstat,
                                'FF3coeff':FF3coeff, 'FF3tstat':FF3tstat,
-                               'FF5coeff':FF5coeff, 'FF5tstat':FF5tstat,
-                               'One':1},
+                               'FF5coeff':FF5coeff, 'FF5tstat':FF5tstat},
     index = ['Intercept', 'MKT', 'SMB', 'HML', 'RMW', 'CMA'])
 
 
@@ -84,7 +88,7 @@ def assetPriceReg(df_stk):
 
 
 # csvファイルを処理する関数
-def calculate(df_stk, predict):
+def calculate(df_stk, predict, df_predict):
   df_stk.head()
   df_stk.drop(['Volume'],axis=1,inplace=True)
   #df_stk.plot()
@@ -94,28 +98,39 @@ def calculate(df_stk, predict):
   df_stk.head()
   df_stk['Returns'].plot()
   #print(df_stk['Returns'].hist(bins=20))
-  df_regOutput = assetPriceReg(df_stk)
+  df_regOutput = assetPriceReg(df_stk, df_predict)
   print(df_regOutput)
+  print(df_predict.df_factors)
  
   #FF3coefと1を入れる空の配列を作成
   array = np.array([])
   one = np.array([])
  
   #np.dot(df_regOutput['FF3coef'], 1が入った配列)をするとnanになるためfor文で計算
+  num = 0
   for i in df_regOutput['FF3coeff']:
- 
+    print(i)
     #iに入ってる値がnanなのかチェック
     if math.isnan(i) == False:
  
       #nanじゃなければそれぞれ値を配列に追加
-      array = np.append(array, i)
-      one = np.append(one, 1)
+      if num == 0:
+        intercept = df_predict.df_factors["Intercept"] * i
+      elif num == 1:
+        mkt = df_predict.df_factors["MKT"] * i
+      elif num == 2:
+        smb = df_predict.df_factors["SMB"] * i
+      else:
+        hml = df_predict.df_factors["HML"] * i
+
+      num += 1
     else:
  
       #nanが来た時点でnp.dotで計算をしてfor文を終了
-      predict.test = np.dot(array, one)
+      predict.test = intercept + mkt + smb + hml
       break
 
+  print(predict.test)
 from pathlib import Path
 import sys
 import os
@@ -129,34 +144,35 @@ fileName = 'df_price_AAPL_2021-12-17' + '.csv'
 pdCsv(fileName)
 
 # 新しい方のアップルのデータを計算
-calculate(df_stk, predict)
+calculate(df_stk, predict, df_predict)
 #print(df_stk['Returns'])
 print(predict.test)
 
-#a = np.array([]) # 1+rにpredict,testをかけた値を保存する配列
-#b = np.array([]) # 実際のリターンの配列
-#for i in df_stk['Returns']:
-  #if math.isnan(i) == True:
-   # continue
-  # ここ間違えてる！！！！！！！！！
-  #c = predict.test * i # aに保存する値を算出
-  #a = np.append(a, c) # cで出力された値を配列に追加
-  #b = np.append(b, i) # iの値を配列に追加
+a = np.array([]) # 1+rにpredict,testをかけた値を保存する配列
+b = np.array([]) # 実際のリターンの配列
+for i in df_stk['Returns']:
+  if math.isnan(i) == True:
+    continue
+  c = predict.test * i # aに保存する値を算出
+  a = np.append(a, c) # cで出力された値を配列に追加
+  b = np.append(b, i) # iの値を配列に追加
 
-#print(a)
-#print(b)
+print(a)
+print(b)
 
 fig, ax = plt.subplots()
 t = np.linspace(0, 10, 1000)
-#ax.plot(a, color="red", label="predict_return")
+ax.grid()
+ax.plot(a, color="red", label="predict_return")
 #ax.plot(b, color="blue", label="aapl_return")
+ax.legend(loc=0)
 fig.tight_layout()
-#plt.show()
+plt.show()
 
 # 古い方のアップルのデータを取得、関数呼び出し
 fileName = 'df_price_AAPL_2021-12-17_old_data' + '.csv'
 pdCsv(fileName)
 
 # 古い方のアップルのデータを計算
-calculate(df_stk, predict)
+#calculate(df_stk, predict, df_predict)
 #print(predict.test)
